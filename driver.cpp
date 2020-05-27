4e7a0a9a638f90fa8c4baf1d0cdf9c7868661be4
georgeo <georgeosabu@gmail.com>
2020-05-27 14:18:10 +0200

    working perfectly

--- driver.cpp
+++ driver.cpp
@@ -2,6 +2,18 @@
 #include <string>
 #include <iostream>
 
+bool converge(std::vector<THNGEO002::State *> s, std::vector<double> prevVals)
+{
+    for (int i = 0; i < prevVals.size(); i++)
+    {
+        if(prevVals[i] != s[i]->getValue()){
+            return false;
+        }
+    }
+    return true;
+    
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -26,51 +38,53 @@ int main(int argc, char *argv[])
     s6.assignNeighbours(&s3, 100);
 
     //Putting all the states in a vector to loop easier
-    std::vector<THNGEO002::State *> states;
-    states.push_back(&s1);
-    states.push_back(&s2);
-    states.push_back(&s3);
-    states.push_back(&s4);
-    states.push_back(&s5);
-    states.push_back(&s6);
+    std::vector<THNGEO002::State *> states = {&s1, &s2, &s3, &s4, &s5, &s6};
+    std::vector<double> prevVals={-1,-1,-1,-1,-1,-1};
 
-    //For each state calculate its (max) value
-    for (int i = 0; i < states.size(); i++)
+    bool check = false;
+    while (check == false)
     {
-        double stateValue = states[i]->getValue();
-        std::cout << "Initial state val for  " << i << " is " << stateValue << std::endl;
-
-        for (int j = 0; j < states[i]->neighbours.size(); j++)
+        for (int st = 0; st < states.size(); st++)
         {
-            double rwrd = states[i]->rewards[j];
-            std::cout << "Rewards from state " << i << " to state " << j << " is " << rwrd << std::endl;
-            double optVal = dReward * (states[i]->neighbours[j]->getValue());
-            std::cout << "optimal value of state " << j << " is " << optVal << std::endl;
-
-            double cVal = rwrd + optVal;
-            if (cVal > stateValue)
-            {
-                stateValue = cVal;
-            }
+           prevVals[st]=states[st]->getValue();
         }
-        std::cout << "new state vaalue  for state " << i << " is " << stateValue << std::endl;
+        
 
-        states[i]->setNewValue(stateValue);
-        std::cout << "================================" << std::endl;
-    }
-    for (int i = 0; i < states.size(); i++)
-    {
-        states[i]->setValue(states[i]->getNewValue());
+
+        //For each state calculate its (max) value
+        for (int i = 0; i < states.size(); i++)
+        {
+            double stateValue = states[i]->getValue();
+            std::cout << "Initial state val for  " << i << " is " << stateValue << std::endl;
+
+            for (int j = 0; j < states[i]->neighbours.size(); j++)
+            {
+                double rwrd = states[i]->rewards[j];
+                std::cout << "Rewards from state " << i << " to state " << j << " is " << rwrd << std::endl;
+                double optVal = dReward * (states[i]->neighbours[j]->getValue());
+                std::cout << "optimal value of state " << j << " is " << optVal << std::endl;
+
+                double cVal = rwrd + optVal;
+                if (cVal > stateValue)
+                {
+                    stateValue = cVal;
+                }
+            }
+            std::cout << "new state vaalue  for state " << i << " is " << stateValue << std::endl;
+
+            states[i]->setNewValue(stateValue);
+            std::cout << "================================" << std::endl;
+        }
+        for (int i = 0; i < states.size(); i++)
+        {
+            states[i]->setValue(states[i]->getNewValue());
+        }
+
+        check=converge(states,prevVals);
     }
 
-    //---------------------------------------------
     for (int i = 0; i < states.size(); i++)
     {
         std::cout << states[i]->getValue() << std::endl;
     }
-
-    // std::cout<<states[0]->neighbours[0]->getValue()<<std::endl;
-    //     std::cout<<states[1]->getValue()<<std::endl;
-
-    // std::cout<<(&states[0]->neighbours[0]==&states[1])<<std::endl;
 }
